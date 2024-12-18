cd0f98e4b88f4c5aaf82657b70906ec83ac5faf0
Edanur Celik <edanur_celik_2003@hotmail.com>
2024-12-18 15:39:31 +0300

    created script.js

--- script.js
+++ script.js
@@ -1,38 +1,38 @@
-const questions=[
+const questions = [
     {
         question: "What does OOP stand for in programming?",
-        answers:[
-            {text:"Object-Oriented Programming", correct:true},
-            {text:"Open Operation Protocol", correct:false},
-            {text:"Online Optimization Process", correct:false},
-            {text:"Object-Oriented Processor", correct:false}
+        answers: [
+            { text: "Object-Oriented Programming", correct: true },
+            { text: "Open Operation Protocol", correct: false },
+            { text: "Online Optimization Process", correct: false },
+            { text: "Object-Oriented Processor", correct: false }
         ]
     },
     {
         question: "Which data structure uses a First In First Out (FIFO) approach?",
-        answers:[
-            {text:"Stack",correct:false},
-            {text:"Queue",correct:true},
-            {text:"Tree",correct:false},
-            {text:"Graph",correct:false}
+        answers: [
+            { text: "Stack", correct: false },
+            { text: "Queue", correct: true },
+            { text: "Tree", correct: false },
+            { text: "Graph", correct: false }
         ]
     },
     {
         question: "In HTML, what tag is used to create a hyperlink?",
-        answers:[
-            {text:"div", correct:false},
-            {text:"span", correct:false},
-            {text:"a", correct:true},
-            {text:"link", correct:false},
+        answers: [
+            { text: "div", correct: false },
+            { text: "span", correct: false },
+            { text: "a", correct: true },
+            { text: "link", correct: false },
         ]
     },
     {
-        question:"What is the keyword used in Python to define a function?",
-        answers:[
-            {text:"func", correct:false},
-            {text:"function", correct:false},
-            {text:"def", correct:true},
-            {text:"lambda", correct:false},
+        question: "What is the keyword used in Python to define a function?",
+        answers: [
+            { text: "func", correct: false },
+            { text: "function", correct: false },
+            { text: "def", correct: true },
+            { text: "lambda", correct: false },
         ]
     }
 ];
@@ -44,18 +44,18 @@ const nextButton = document.getElementById("next-btn");
 let currentQuestionIndex = 0;
 let score = 0;
 
-function startQuiz(){
+function startQuiz() {
     currentQuestionIndex = 0;
     score = 0;
-    nextButton.innerHTML= "Next";
+    nextButton.innerHTML = "Next";
     showQuestion();
 }
 
-function showQuestion(){
+function showQuestion() {
     resetState();
     let currentQuestion = questions[currentQuestionIndex];
     let questionNo = currentQuestionIndex + 1;
-    questionElement.innerHTML = questionNo + ". " + currentQuestion.question ; 
+    questionElement.innerHTML = questionNo + ". " + currentQuestion.question;
 
     currentQuestion.answers.forEach(answer => {
         const button = document.createElement("button");
@@ -63,63 +63,63 @@ function showQuestion(){
         button.classList.add("btn");
         answerButtons.appendChild(button);
 
-        if(answer.correct){
+        if (answer.correct) {
             button.dataset.correct = answer.correct;
         }
 
-        button.addEventListener("click" , selectAnswer);
+        button.addEventListener("click", selectAnswer);
     });
 }
 
-function  resetState(){
-    nextButton.style.display="none";
-    while(answerButtons.firstChild){
+function resetState() {
+    nextButton.style.display = "none";
+    while (answerButtons.firstChild) {
         answerButtons.removeChild(answerButtons.firstChild);
     }
 }
 
-function selectAnswer(e){
+function selectAnswer(e) {
     const selectedBtn = e.target;
     const isCorrect = selectedBtn.dataset.correct === "true";
-    if(isCorrect){
+    if (isCorrect) {
         selectedBtn.classList.add("correct");
         score++;
     }
-    else{
+    else {
         selectedBtn.classList.add("incorrect");
     }
-    Array.from(answerButtons.children).forEach(button =>{
-        if(button.dataset.correct === "true"){
+    Array.from(answerButtons.children).forEach(button => {
+        if (button.dataset.correct === "true") {
             button.classList.add("correct");
         }
-        button.disabled=true;
+        button.disabled = true;
     });
     nextButton.style.display = "block";
 
 }
 
-function handleNextButton(){
+function handleNextButton() {
     currentQuestionIndex++;
-    if(currentQuestionIndex < questions.length){
+    if (currentQuestionIndex < questions.length) {
         showQuestion();
     }
-    else{
+    else {
         showScore();
     }
 }
 
-function showScore(){
+function showScore() {
     resetState();
-    questionElement.innerHTML= "You scored " + score +  " out of " + questions.length + "!";
+    questionElement.innerHTML = "You scored " + score + " out of " + questions.length + "!";
     nextButton.innerHTML = "Play Again";
     nextButton.style.display = "block";
 }
 
 nextButton.addEventListener("click", () => {
-    if(currentQuestionIndex < questions.length){
-      handleNextButton();
+    if (currentQuestionIndex < questions.length) {
+        handleNextButton();
     }
-    else{
+    else {
         startQuiz();
     }
 });
